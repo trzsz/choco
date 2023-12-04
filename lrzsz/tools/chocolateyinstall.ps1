f00822044e35ac008e7bd6a5108d0ca158d8f762
Lonny Wong <lonnywong@qq.com>
2023-12-04 23:22:12 +0800

    lrzsz 0.12.21rc

---
+++ lrzsz/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url64      = 'https://github.com/trzsz/lrzsz-win32/releases/download/v0.12.21rc/lrzsz_0.12.21rc_windows_x86_64.zip'
+$checksum64 = 'dace2db172b1b5a1e5998692760a75355b254e1826718d88bae7fa3a6de3c325'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url64bit      = $url64
+  checksum64    = $checksum64
+  checksumType64= 'sha256'
+  specificFolder= 'lrzsz_*'
+}
+
+Install-ChocolateyZipPackage @packageArgs
