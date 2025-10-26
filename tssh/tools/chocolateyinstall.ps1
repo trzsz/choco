49a978072ed3a1ca64df76c54bd60001b96ae2d7
Lonny Wong <lonnywong@qq.com>
2025-10-26 10:28:34 +0800

    tssh v0.1.23

--- tssh/tools/chocolateyinstall.ps1
+++ tssh/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 $ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url32      = 'https://github.com/trzsz/trzsz-ssh/releases/download/v0.1.22/tssh_0.1.22_windows_i386.zip'
-$url64      = 'https://github.com/trzsz/trzsz-ssh/releases/download/v0.1.22/tssh_0.1.22_windows_x86_64.zip'
-$checksum32 = '94afb65a192ff2a2b72b590e7ed1c27f261741ba4764df0dc6c29713543afc66'
-$checksum64 = 'c80109640cced9bec668a32a5de79d191dd3a551e44f1173798bef220f6d7321'
+$url32      = 'https://github.com/trzsz/trzsz-ssh/releases/download/v0.1.23/tssh_0.1.23_windows_i386.zip'
+$url64      = 'https://github.com/trzsz/trzsz-ssh/releases/download/v0.1.23/tssh_0.1.23_windows_x86_64.zip'
+$checksum32 = '18dcd010f2b19b57d071233beb19de5176a58da5c699ce061fb63002d92ea3fd'
+$checksum64 = '027254b08e62e21e109b99c003d8a75cd096f4c052d36f09fe29180f90f07c5a'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
